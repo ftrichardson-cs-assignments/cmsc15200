d8ff72752b5b7d0898dc0e642c333bcf8405b820
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-02-20 20:21:59 -0600

    final version of pa#3 ready for submission

--- pa/pa3/src/scheduler.c
+++ pa/pa3/src/scheduler.c
@@ -12,9 +12,15 @@
 
 int run_tasks(task_manager_t *tm, int time_slices[], int *total_time_ptr)
 {
-  // YOUR CODE HERE
-  // Replace 0 with an appropriate return value
-  return 0;
+	int num_tasks = 0;
+	*total_time_ptr = 0;
+
+	while (!tm_is_empty(tm)) 
+	{
+		*total_time_ptr += tm_run_cycle(tm, time_slices);
+		num_tasks++;
+	}
+	return num_tasks;
 }
 
 
