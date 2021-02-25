856a8e1bae205d388138c048c473d1b4456ebf48
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-02-25 13:32:05 -0600

    confused about get_completions and free_trie

--- pa/pa4/src/trie.c
+++ pa/pa4/src/trie.c
@@ -10,6 +10,8 @@
 
 #include "trie.h"
 
+#define max(i, j) (i > j ? i : j) // used in add_word()
+
 /* Create a new tnode_t
  *
  * Returns: a pointer to a tnode_t
@@ -20,6 +22,10 @@ tnode_t *create_trie_node()
     trie->final = false;
     trie->count = 0;
     trie->longest = 0;
+    for (int i = 0; i < NUM_CHARACTERS; i++) 
+    {
+        trie->next[i] = NULL;
+    }
     
     return trie;
 }
@@ -37,24 +43,12 @@ void free_trie(tnode_t *trie)
 
     for (int i = 0; i < NUM_CHARACTERS; i++)
     {
-        free_trie(trie->next[i]);
+        if (trie->next[i] != NULL) 
+        {
+            free_trie(trie->next[i]);
+        }
     }
     free(trie);
-}
-
-int longest_completion(int i, int j)
-{
-    if (i <= j)
-    {
-        return j;
-    }    
-
-    return i;
-}
-
-int letter_to_index(char letter)
-{
-    return (int) letter - (int) 'a';
 }
 
 /* Add a word to a trie
@@ -62,56 +56,64 @@ int letter_to_index(char letter)
  * trie: a pointer to the trie
  * word: the word to add
  */
-
 void add_word(tnode_t *trie, char *word)
 {
     trie->count++;
-    trie->longest = longest_completion(trie->longest, strlen(word));
+    trie->longest = max(trie->longest, strlen(word));
 
     for (int n = 0; n < strlen(word); n++)
     {
         char letter = word[n];
-        int letter_index = letter_to_index(letter);
-        int completion_length = strlen(word) - (n + 1);
+        int ASCII = (int) letter - (int) 'a';
+        int len_completion = strlen(word) - (n + 1);
 
-        if (trie->next[letter_index] == NULL)
+        if (trie->next[ASCII] == NULL)
         {
-            trie->next[letter_index] = create_trie_node();
-            trie->next[letter_index]->count++;
-            trie->next[letter_index]->longest = completion_length;
+            trie->next[ASCII] = create_trie_node();
+            trie->next[ASCII]->count++;
+            trie->next[ASCII]->longest = len_completion;
+
         } else 
         {
-            trie->next[letter_index]->count++;
-            trie->next[letter_index]->longest = longest_completion(trie->next[letter_index]->longest, completion_length);
+            trie->next[ASCII]->count++;
+            trie->next[ASCII]->longest = max(trie->next[ASCII]->longest, len_completion);
         }
 
+        /* Validate trie->next[ASCII] once end of word reached */
         if (n == strlen(word) - 1) 
         {
-            trie->next[letter_index]->final = true;
+            trie->next[ASCII]->final = true;
         }
 
-        trie = trie->next[letter_index];
+        trie = trie->next[ASCII];
     }
 }
 
-tnode_t *prefix_trie(tnode_t* trie, char *prefix) 
+/* traverse_trie: traverses a trie
+ * 
+ * trie: a pointer to the trie
+ * prefix: the prefix to complete
+ *
+ * Returns: pointer to trie according to prefix
+ */
+tnode_t *traverse_trie(tnode_t* trie, char *prefix) 
 {
     for (int n = 0; n < strlen(prefix); n++) 
     {
         char letter = prefix[n];
-        int letter_index = letter_to_index(letter);
+        int ASCII = (int) letter - (int) 'a';
 
-        if (trie->next[letter_index] == NULL && !trie->final) 
+        if (trie->next[ASCII] == NULL && !trie->final) 
         {
             break;
         }
 
         if (n == strlen(prefix) - 1)
         {
-            return trie->next[letter_index];
+            return trie->next[ASCII];
         }
 
-        trie = trie->next[letter_index];
+        trie = trie->next[ASCII];
 
     }
     return NULL;
@@ -126,24 +128,14 @@ tnode_t *prefix_trie(tnode_t* trie, char *prefix)
  */ 
 bool is_a_word(tnode_t *trie, char *word)
 {
-    for (int n = 0; n < strlen(word); n++)
+    tnode_t* word_trie = traverse_trie(trie, word);
+    
+    if (!word_trie) 
     {
-        char letter = word[n];
-        int letter_index = letter_to_index(letter);
-
-        if (trie->next[letter_index] == NULL)
-        {
-            return false;
-        }
-
-        if (n == strlen(word) - 1) 
-        {
-            return trie->next[letter_index]->final;
-        }
-
-        trie = trie->next[letter_index];
+        return false;
     }
-    return false;
+
+    return word_trie->final;
 }
 
 /* Get the number of completions
@@ -155,24 +147,29 @@ bool is_a_word(tnode_t *trie, char *word)
  */ 
 int num_completions(tnode_t *trie, char *prefix)
 {
-    for (int n = 0; n < strlen(prefix); n++) 
+    tnode_t* prefix_trie = traverse_trie(trie, prefix);
+    
+    if (!prefix_trie) 
     {
-        char letter = prefix[n];
-        int letter_index = letter_to_index(letter);
+        return 0;
+    }
 
-        if (trie->next[letter_index] == NULL && !trie->final) 
+    return prefix_trie->count;
+}
+
+char* generate_completions(tnode_t *trie, char *prefix) 
+{
+    tnode_t* prefix_trie = traverse_trie(trie, prefix);
+
+    for (int i = 0; i < NUM_CHARACTERS; i++)
+    {
+        if (trie->next[i] == NULL)
         {
             break;
         }
 
-        if (n == strlen(prefix) - 1)
-        {
-            return trie->next[letter_index]->count;
-        }
 
-        trie = trie->next[letter_index];
     }
-    return 0;
 }
 
 
@@ -185,17 +182,11 @@ int num_completions(tnode_t *trie, char *prefix)
  */ 
 char **get_completions(tnode_t *trie, char *prefix)
 {
-    int len_completions = num_completions(trie, prefix);
-    char** completions = (char**)malloc((len_completions + 1) * sizeof(char*));
+    tnode_t* prefix_trie = traverse_trie(trie, prefix);
+    char** completions = (char**)malloc((num_completions(trie, prefix) + 1) * sizeof(char*));
 
-    if (len_completions == 0)
+    if (prefix && !prefix_trie)
     {
-        return completions;
+        return NULL;
     }
-    
-    for (int n = 0; n < len_completions; n++)
-    {
-
-    }
-
 }
