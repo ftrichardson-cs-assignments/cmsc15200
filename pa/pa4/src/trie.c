07d80bc56cad428ba7d29d859635be974ef55272
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-02-27 21:25:16 -0600

    very confused on get_completions

--- pa/pa4/src/trie.c
+++ pa/pa4/src/trie.c
@@ -36,11 +36,6 @@ tnode_t *create_trie_node()
  */ 
 void free_trie(tnode_t *trie)
 {
-    if (!trie)
-    {
-        return;
-    }
-
     for (int i = 0; i < NUM_CHARACTERS; i++)
     {
         if (trie->next[i] != NULL) 
@@ -148,7 +143,12 @@ bool is_a_word(tnode_t *trie, char *word)
 int num_completions(tnode_t *trie, char *prefix)
 {
     tnode_t* prefix_trie = traverse_trie(trie, prefix);
-    
+   
+    if (!strcmp(prefix, ""))
+    {
+        return trie->count;
+    }
+     
     if (!prefix_trie) 
     {
         return 0;
@@ -157,18 +157,46 @@ int num_completions(tnode_t *trie, char *prefix)
     return prefix_trie->count;
 }
 
-char* generate_completions(tnode_t *trie, char *prefix) 
-{
-    tnode_t* prefix_trie = traverse_trie(trie, prefix);
 
-    for (int i = 0; i < NUM_CHARACTERS; i++)
+
+/* The get_completions function I am struggling with greatly because recursion is still a
+concept that I struggle to understand, especially with more complex applications
+of recursion. I have commented some of my code so that it will be easier to see where 
+I am misunderstanding my code and/or my logic to solve this problem is wrong. I think if I'm
+able to solve this problem myself, the concept will be much clearer to me, so any hints you could give me
+about my implementation would be greatly appreciated! Thank you!!! */
+
+void completions_array(tnode_t* trie, char *temp_str, char **completions, int *index_ptr) 
+{
+    if (!trie) //Base case (if trie->next[n] == NULL then !trie is true)
     {
-        if (trie->next[i] == NULL)
+        return;
+    }
+     
+    int temp_str_index = 0; //Separate index for temp_string
+
+    for (int n = 0; n < NUM_CHARACTERS; n++)
+    {
+        completions_array(trie->next[n], temp_string, completions, index_ptr); //Is this where I should be recursing?
+        char letter = (char) (n + (int) 'a');
+        
+        if (trie->next[n] != NULL && !trie->final)
         {
-            break;
+            temp_string[temp_str_index] = letter;
+            temp_str_index++;
         }
 
+        if (trie->next[n] == NULL && trie->final)
+        {
+            temp_string[str_index] = letter;
+            temp_string[str_index + 1] = '\0';
+            temp_string = realloc(temp_string, (strlen(temp_string) + 1) * sizeof(char));
+            completions[*index_ptr] = temp_string;
+            (*index_ptr)++;
+        }
 
+        /*I know this code doesn't currently handle a case such as "the" if "their" is in trie, but I 
+        want to make sure I understand the recursion first before proceeding */
     }
 }
 
@@ -182,11 +210,17 @@ char* generate_completions(tnode_t *trie, char *prefix)
  */ 
 char **get_completions(tnode_t *trie, char *prefix)
 {
-    tnode_t* prefix_trie = traverse_trie(trie, prefix);
-    char** completions = (char**)malloc((num_completions(trie, prefix) + 1) * sizeof(char*));
+    tnode_t* prefix_trie = traverse_trie(trie, prefix); // Brings me to the correct trie
+    char** completions = (char**)malloc(num_completions(trie, prefix) * sizeof(char*));
+    char* temp_string = (char*)malloc((prefix_trie->longest + 1) * sizeof(char));
+    int index = 0; // Index for completions
 
     if (prefix && !prefix_trie)
     {
-        return NULL;
+        return NULL; //Case of NO completions
     }
+
+    completions_array(prefix_trie, temp_string, completions, &index);
+
+    return completions; //After call, I return the filled array (which obviously is not currently accurate)
 }
