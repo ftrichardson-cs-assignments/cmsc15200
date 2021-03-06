9b8f073b9f38a1f2ce676d021371234ba9325e81
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-03-06 10:48:13 -0600

    35/38

--- pa/pa5/src/scheduler.c
+++ pa/pa5/src/scheduler.c
@@ -13,10 +13,6 @@
 
 /********* Helper functions *********/
 
-// Put your helper functions (if any) here.
-
-/********* Required function *********/
-
 /* simulate_cores: simulates task execution in a set of cores
  *
  * tm: a pointer to a task manager w/ a set of tasks to complete
@@ -30,7 +26,69 @@
  */
 int simulate_cores(task_manager_t *tm, int num_cores, int time_slice, int *total_time_ptr)
 {
-    // YOUR CODE HERE
-    // REPLACE 0 WITH A SUITABLE RETURN VALUE
-    return 0;
+    *total_time_ptr = 0;
+    int num_cycles = 0;
+    task_t** cores = (task_t**)malloc(num_cores * sizeof(task_t*));
+
+    // Set every element in cores to NULL
+    for (int n = 0; n < num_cores; n++) 
+    {
+        cores[n] = NULL;
+    }
+
+    while (!tm_is_empty(tm)) 
+    {
+        // Fill cores
+        for (int n = 0; n < num_cores; n++) 
+        {
+            if (cores[n] == NULL) 
+            {
+                cores[n] = tm_remove_most_urgent_task(tm);
+            }
+        }
+
+        // Execute tasks
+        for (int n = 0; n < num_cores; n++) 
+        {
+            int execution_time;
+
+            if (cores[n] == NULL) 
+            {
+                continue;
+            }
+
+            enum task_status status = execute_task(cores[n], time_slice, &execution_time);
+            *total_time_ptr += execution_time;
+
+            if (status == TASK_DONE) 
+            {
+                free(cores[n]);
+                cores[n] = NULL;
+            }
+
+            if (status == TASK_RESCHEDULE) 
+            {
+                tm_add_task(tm, cores[n]);
+                cores[n] = NULL;
+            }
+
+            if (status == TASK_KEEP) 
+            {
+                continue;
+            }
+        }
+        num_cycles++;
+    }
+
+    // Check for any task remaining in core once task manager empty
+    for (int n = 0; n < num_cores; n++) 
+    {
+        if (cores[n] != NULL) 
+        {
+            int execution_time;
+            enum task_status status = execute_task(cores[n], time_slice, &execution_time);
+            *total_time_ptr += execution_time;
+        }
+    }
+    return num_cycles;
 }
