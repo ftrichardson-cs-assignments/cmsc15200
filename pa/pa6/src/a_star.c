6841901b194076f3c7e71cacd310205b4d31641c
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-03-17 11:01:42 -0500

    41 errors running valgrind

--- pa/pa6/src/a_star.c
+++ pa/pa6/src/a_star.c
@@ -15,8 +15,27 @@
  */ 
 graph_t *graph_create(int num_nodes)
 {
-    //  YOUR CODE HERE
-    return NULL;
+    graph_t* empty_graph = (graph_t*)malloc(sizeof(graph_t));
+    if (empty_graph == NULL) 
+    {
+        fprintf(stderr, "graph_create: malloc failure\n");
+        exit(1);
+    }
+
+    empty_graph->num_nodes = num_nodes;
+    empty_graph->nodes = (node_t**)malloc(num_nodes * sizeof(node_t*));
+    if (empty_graph->nodes == NULL) 
+    {
+        fprintf(stderr, "graph_create: malloc failure\n");
+        exit(1);
+    }
+
+    for (int n = 0; n < num_nodes; n++) 
+    {
+        empty_graph->nodes[n] = NULL;
+    }
+
+    return empty_graph;
 }
 
 /* node_create: create a graph node
@@ -31,8 +50,62 @@ graph_t *graph_create(int num_nodes)
 void node_create(graph_t *graph, int node_num, char *city_name, 
                  double latitude, double longitude)
 {    
-    //  YOUR CODE HERE
-    return;
+    graph->nodes[node_num] = (node_t*)malloc(sizeof(node_t));
+    if (graph->nodes[node_num] == NULL) 
+    {
+        fprintf(stderr, "node_create: malloc failure\n");
+        exit(1);
+    }
+
+    // City
+    graph->nodes[node_num]->node_num = node_num;
+    graph->nodes[node_num]->city_name = city_name;
+    graph->nodes[node_num]->latitude = latitude;
+    graph->nodes[node_num]->longitude = longitude;
+
+    // Neighbors
+    graph->nodes[node_num]->neighbors = NULL;
+
+    // A* fields
+    graph->nodes[node_num]->parent = NULL;
+    graph->nodes[node_num]->f_cost = 0;
+    graph->nodes[node_num]->g_cost = 0;
+    graph->nodes[node_num]->h_cost = 0;
+}
+
+/* add_edge_h: adds a new neighbor with specified data to next open slot
+ *
+ * graph: the graph
+ * node_num: node number
+ * data: the data to add to new neighbor
+ */ 
+void add_edge_h(graph_t *graph, int node_num, int data) 
+{
+    intlist_t* new_neighbor = (intlist_t*)malloc(sizeof(intlist_t*));
+    if (new_neighbor == NULL)
+    {
+        fprintf(stderr, "add_edge_h: malloc failure\n");
+        exit(1);
+    }
+
+    new_neighbor->num = data;
+    new_neighbor->next = NULL;
+
+    // Check if neighbors empty
+    if (graph->nodes[node_num]->neighbors == NULL) 
+    {
+        graph->nodes[node_num]->neighbors = new_neighbor;
+        return;
+    }
+
+    intlist_t* neighbor = graph->nodes[node_num]->neighbors;
+        
+    while (neighbor->next != NULL) 
+    {
+        neighbor = neighbor->next;
+    }
+
+    neighbor->next = new_neighbor;
 }
 
 /* add_edge: add an edge between two nodes
@@ -44,21 +117,57 @@ void node_create(graph_t *graph, int node_num, char *city_name,
  */ 
 void add_edge(graph_t* graph, int node_num1, int node_num2)
 {
-    //  YOUR CODE HERE
-    return;
+    add_edge_h(graph, node_num1, node_num2);
+    add_edge_h(graph, node_num2, node_num1);
+}
+
+/********* FREE FUNCTIONS *********/
+
+/* neighbors_free: frees node's neighbor(s)
+ *
+ * neighbors: linked list of neighbor(s)
+ */ 
+void neighbors_free(intlist_t* neighbors) 
+{
+    intlist_t* tmp;
+    
+    while (neighbors != NULL) 
+    {
+        tmp = neighbors;
+        neighbors = neighbors->next;
+        free(tmp);
+    }
 }
 
 /* graph_free: free a graph and its nodes
  *
  * graph: the graph
  */ 
-void graph_free(graph_t* graph)
+void graph_free(graph_t *graph)
 {
-    //  YOUR CODE HERE
-    return;
+    for (int i = 0; i < graph->num_nodes; i++)
+    {
+        if (graph->nodes[i] != NULL)
+        {
+            neighbors_free(graph->nodes[i]->neighbors);
+            free(graph->nodes[i]);
+        }
+    }
+    free(graph->nodes);
+    free(graph);
 }
 
 /********* A* SEARCH *********/
+
+double euclidean_distance(graph_t *graph, int node_num_1, int node_num_2) 
+{
+    double latitude_1 = graph->nodes[node_num_1]->latitude;
+    double longitude_1 = graph->nodes[node_num_1]->longitude;
+    double latitude_2 = graph->nodes[node_num_2]->latitude;
+    double longitude_2 = graph->nodes[node_num_2]->longitude;
+
+    return sqrt(pow(latitude_1 - latitude_2, 2.0) + pow(longitude_1 - longitude_2, 2.0));
+}
 
 /* a_star: performs A* search
  *
@@ -70,6 +179,5 @@ void graph_free(graph_t* graph)
  */ 
 double a_star(graph_t *graph, int start_node_num, int end_node_num)
 {
-    //  YOUR CODE HERE
     return 0;
 }
