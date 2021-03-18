5495e12fc71cf2c68cf83033bed858af66086490
Flynn Richardson <frichardson@cs.uchicago.edu>
2021-03-17 21:03:30 -0500

    failing a_star

--- pa/pa6/src/a_star.c
+++ pa/pa6/src/a_star.c
@@ -7,10 +7,13 @@
 #include "util.h"
 #include "a_star.h"
 
+// For A* SEARCH
+enum path_costs {H_COST, G_COST};
+
 /********* GRAPH *********/
 
 /* graph_create: create a graph
- *
+ 
  * Returns: a graph
  */ 
 graph_t *graph_create(int num_nodes)
@@ -73,11 +76,11 @@ void node_create(graph_t *graph, int node_num, char *city_name,
     graph->nodes[node_num]->h_cost = 0;
 }
 
-/* add_edge_h: adds a new neighbor with specified data to next open slot
+/* add_edge_h: adds new neighbor with specified data to end of neighbors
  *
  * graph: the graph
  * node_num: node number
- * data: the data to add to new neighbor
+ * data: num of new neighbor
  */ 
 void add_edge_h(graph_t *graph, int node_num, int data) 
 {
@@ -126,7 +129,7 @@ void add_edge(graph_t* graph, int node_num1, int node_num2)
 
 /* neighbors_free: frees node's neighbor(s)
  *
- * neighbors: linked list of neighbor(s)
+ * neighbors: linked list containing neighbor(s)
  */ 
 void neighbors_free(intlist_t* neighbors) 
 {
@@ -160,14 +163,63 @@ void graph_free(graph_t *graph)
 
 /********* A* SEARCH *********/
 
-double euclidean_distance(graph_t *graph, int node_num_1, int node_num_2) 
+/* euclidean_distance: calculates euclidean distance
+ * 
+ * graph: the graph
+ * node_num1: the first node
+ * node_num2: the second node
+ *
+ * Returns: euclidean distance
+ */
+double euclidean_distance(graph_t *graph, int node_num1, int node_num2) 
 {
-    double latitude_1 = graph->nodes[node_num_1]->latitude;
-    double longitude_1 = graph->nodes[node_num_1]->longitude;
-    double latitude_2 = graph->nodes[node_num_2]->latitude;
-    double longitude_2 = graph->nodes[node_num_2]->longitude;
+    double latitude1 = graph->nodes[node_num1]->latitude;
+    double longitude1 = graph->nodes[node_num1]->longitude;
+    double latitude2 = graph->nodes[node_num2]->latitude;
+    double longitude2 = graph->nodes[node_num2]->longitude;
 
-    return sqrt(pow(latitude_1 - latitude_2, 2.0) + pow(longitude_1 - longitude_2, 2.0));
+    return sqrt(pow(latitude1 - latitude2, 2.0) + pow(longitude1 - longitude2, 2.0));
+}
+
+/* set parent: sets parent field of node
+ *
+ * graph: the graph
+ * node_num_parent: the parent's node number
+ * node_num_child: the child's node number
+ */
+void set_parent(graph_t *graph, int node_num_parent, int node_num_child) 
+{
+    graph->nodes[node_num_child]->parent = graph->nodes[node_num_parent];
+}
+
+
+double g_cost_calculate(graph_t* graph, int start_node_num, int node_num) 
+{
+    if (node_num == start_node_num) 
+    {
+        return 0;
+    }
+
+    int parent = graph->nodes[node_num]->parent->node_num;
+    double parent_to_node_cost = euclidean_distance(graph, start_node_num, node_num);
+
+    return graph->nodes[node_num]->g_cost + parent_to_node_cost;
+}
+
+/* set_path_costs: sets the h_cost, g_cost and f_costs for node 
+ * 
+ * graph: the graph
+ * node_num: node for which to set path costs 
+ * h_cost: the h_cost
+ * g_cost: the g_cost
+ * f_cost: the f_cost
+ */
+void set_path_costs(graph_t* graph, int node_num, double h_cost, 
+                    double g_cost, double f_cost) 
+{
+    graph->nodes[node_num]->h_cost = h_cost;
+    graph->nodes[node_num]->g_cost = g_cost;
+    graph->nodes[node_num]->f_cost = f_cost;
 }
 
 /* a_star: performs A* search
@@ -180,7 +232,71 @@ double euclidean_distance(graph_t *graph, int node_num_1, int node_num_2)
  */ 
 double a_star(graph_t *graph, int start_node_num, int end_node_num)
 {
-    // Priority queue
+    queue_t* open_set = queue_create();
+    set_t* closed_set = set_create();
 
-    // Closed set
+    // Set start node path costs
+    double h_cost = euclidean_distance(graph, start_node_num, end_node_num);
+    double g_cost = g_cost_calculate(graph, start_node_num, start_node_num);
+    double f_cost = h_cost + g_cost;
+    set_path_costs(graph, start_node_num, h_cost, g_cost, f_cost);
+
+    // Add to open set
+    queue_add(open_set, start_node_num, f_cost);
+
+    // Open set
+    while (!queue_is_empty(open_set))
+    {
+        queue_print(open_set);
+        int current_node = queue_remove(open_set);
+        queue_print(open_set);
+
+        // Break if current_node
+        if (current_node == end_node_num) 
+        {
+            break;
+        }
+
+        intlist_t* current_neighbor = graph->nodes[current_node]->neighbors;
+
+        while (current_neighbor != NULL)
+        {
+            int node_num = current_neighbor->num;
+
+            // Check if parent field NULL
+            if (graph->nodes[node_num]->parent == NULL) 
+            {
+                set_parent(graph, current_node, node_num);
+            }
+
+            double h_cost = euclidean_distance(graph, node_num, end_node_num);
+            double g_cost = g_cost_calculate(graph, start_node_num, node_num);
+            double f_cost = h_cost + g_cost;
+
+            if (set_query(closed_set, node_num) || (queue_query(open_set, node_num)
+                && graph->nodes[node_num]->f_cost < f_cost)) 
+            {
+                current_neighbor = current_neighbor->next;
+                continue;
+
+            } else
+            {
+                set_path_costs(graph, node_num, h_cost, g_cost, f_cost);
+                set_parent(graph, current_node, node_num);
+                queue_add(open_set, node_num, f_cost);
+                queue_print(open_set);
+
+                current_neighbor = current_neighbor->next;
+            }
+        }
+        set_add(closed_set, current_node);
+        set_print(closed_set);
+    }
+
+    if (graph->nodes[end_node_num]->f_cost == 0) 
+    {
+        return -1;
+    }
+
+    return graph->nodes[end_node_num]->f_cost;
 }
